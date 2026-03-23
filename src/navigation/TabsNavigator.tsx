c2ca9eeb15c313faf0420500b2f3be108fdc74f4
LAPTOP_DEV\funes <funes_garcia@hotmail.com>
2026-03-22 18:45:21 -0600

    Agrega cambio de idioma en TabsNavigator y pantallas completas

--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text } from 'react-native';
+import { useLanguage } from '../contexts/LanguageContext';
 
 import HomeScreen from '../screens/HomeScreen';
 import VisitorsScreen from '../screens/VisitorsScreen';
@@ -17,6 +18,8 @@ export type TabParamList = {
 const Tab = createBottomTabNavigator<TabParamList>();
 
 const TabsNavigator = () => {
+  const { t } = useLanguage();
+
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
@@ -40,10 +43,10 @@ const TabsNavigator = () => {
         headerShown: false,
       })}
     >
-      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Inicio' }} />
-      <Tab.Screen name="Visitors" component={VisitorsScreen} options={{ title: 'Visitantes' }} />
-      <Tab.Screen name="History" component={HistoryScreen} options={{ title: 'Historial' }} />
-      <Tab.Screen name="Profile" component={ProfileScreen} options={{ title: 'Perfil' }} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ title: t('welcome') }} />
+      <Tab.Screen name="Visitors" component={VisitorsScreen} options={{ title: t('visitors') }} />
+      <Tab.Screen name="History" component={HistoryScreen} options={{ title: t('visitHistory') }} />
+      <Tab.Screen name="Profile" component={ProfileScreen} options={{ title: t('profile') }} />
     </Tab.Navigator>
   );
 };
